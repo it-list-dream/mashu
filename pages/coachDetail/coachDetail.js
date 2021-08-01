57d1f89b3aafc650c45ff9e16c2f1a58d2089ffb
lj <luo_jun123123@163.com>
2021-08-01 17:01:07 +0800

    second commit

--- pages/coachDetail/coachDetail.js
+++ pages/coachDetail/coachDetail.js
@@ -1,11 +1,11 @@
-// pages/courseBooking/courseBooking.js
+// pages/coachDetail/coachDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     useCoupon:true
+
   },
 
   /**
--- pages/courseDetail/courseDetail.js
+++ pages/courseDetail/courseDetail.js
@@ -1,11 +1,11 @@
-// pages/courseBooking/courseBooking.js
+// pages/courseDetail/courseDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     useCoupon:true
+
   },
 
   /**
